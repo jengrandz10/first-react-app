a953fc93192ffb0f4300aafd77e1332f3b3f22eb
JenGrandz <jennygrande1010@gmail.com>
2020-08-20 15:54:02 +0800

    Updates

---
+++ precache-manifest.5f7368bc424d20f5caaf620fef29b3be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde5e0222ee01903fc61bc3d62219dd2",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "32514eec13a17b2e22c8",
+    "url": "/first-react-app/static/css/main.3238f103.chunk.css"
+  },
+  {
+    "revision": "7acefadd100fb794b79d",
+    "url": "/first-react-app/static/js/2.5de63503.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.5de63503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32514eec13a17b2e22c8",
+    "url": "/first-react-app/static/js/main.61a1c68e.chunk.js"
+  },
+  {
+    "revision": "2c4ef5ceae152c5e1a75",
+    "url": "/first-react-app/static/js/runtime-main.9f029547.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
